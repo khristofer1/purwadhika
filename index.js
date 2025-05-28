93ff73e4d69dd8e4f1d2c611a2fc24c39e8f7d92
khristofer1 <khristofer@yahoo.com>
2025-05-28 11:27:35 +0700

    Looping

---
+++ index.js
@@ -0,0 +1,14 @@
+// Sebenarnya, pernyataan-pernyataan di for loop tidak perlu diisi
+// let i = 1;
+// for (;;) {
+//     console.log(i++);
+//     if (i > 10) {
+//         break;
+//     }
+// }
+
+let i = 0;
+for (let i = 0; i < 10; i++) {
+    console.log(i);
+}
+console.log(i);
